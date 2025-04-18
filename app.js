ef0ca83c59a3f9e270bc6096428b14e47ad1f1c4
casherka <76988015+casherka@users.noreply.github.com>
2025-04-18 23:45:06 +0500

    Clear old

--- app.js
+++
@@ -1,6 +0,0 @@
-const config = require('./config.json');
-const PluginsLoader = require("./scripts/pluginsLoader.js")
-
-// Загрузка плагинов
-const pluginsLoader = new PluginsLoader()
-pluginsLoader.loadPlugins(config)
